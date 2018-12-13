df27e2fbd506dfe4b80c15550949efc69b27f803
andsju <anders.sjunnesson@gmail.com>
2018-12-13 12:12:00 +0100

    Express server

--- server.js
+++ server.js
@@ -13,20 +13,22 @@ const logAccessStream = require('./config/logger').logAccessStream;
 
 var app = express();
 
+/* --------------------------------------------------
+ express middleware
+ -------------------------------------------------- */
+
 // log all requests to file
-app.use(morgan('tiny', {stream: logAccessStream}));
+app.use(morgan('tiny', {
+  stream: logAccessStream
+}));
 
 // log 4xx and 5xx responses to console
 app.use(morgan('dev', {
-  skip: function (req, res) { return res.statusCode < 400 }
+  skip: function (req, res) {
+    return res.statusCode < 400
+  }
 }));
 
-
-/* app.get('/', function(req, res){
-  res.send('Hello World');
-  logger.info('Hello world get root');
-});
- */
 
 /* --------------------------------------------------
  routes
@@ -35,20 +37,27 @@ app.use("/", require("./routes/index.js"));
 app.use("/about", require("./routes/about.js"));
 
 /* --------------------------------------------------
- 404
+ 404 - not found
+ Express has executed all middleware functions and routes, and found that none of them responded.
  -------------------------------------------------- */
-app.use(function(req, res, next) {
-  return res.status(404).send({ message: '404 '+req.url+' Not found.' });
+app.use(function (req, res, next) {
+  return res.status(404).send({
+    message: '404 ' + req.url + ' Not found.'
+  });
 });
 
 /* --------------------------------------------------
  500 - server error
+ Express error handling takes a fourth argument as an error: (err, req, res, next)
  -------------------------------------------------- */
-app.use(function(err, req, res, next) {
-  return res.status(500).send({ error: err });
+app.use(function (err, req, res, next) {
+  logger.debug(err);
+  return res.status(500).send({
+    error: err
+  });
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   // console.log("Express server running");
   logger.info('Express server running - Hello world');
 });
